353610665eb6e94afdb33f5d1b70e2463e04a42f
sreemukhareddy <rsreemukha@gmail.com>
2026-04-13 19:59:26 +0530

    cc

---
+++ eb-mcp-client/src/test/java/com/eb/mcp/EbMcpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eb.mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbMcpClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
